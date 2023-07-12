205e4c7636a42411bedc6cfbf045dff032b029d0
gamrani younes <gamrani.younes@gmail.com>
2023-07-12 15:07:53 -0700

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*
+*
+*main  prints the name of the program
+ 
+*@argv : array of arguments 
+*@argv : array of arguments
+*
+*
+return:0 ( suceces)
+**/
+int main(int argc , char* argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return 0;
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*
+*
+*main  prints the name of the program
+ 
+*argv : array of arguments 
+*argv : array of arguments
+*
+*
+return:0 ( suceces)
+**/
+int main( 
+
+
+0x0A. C - argc, argv
