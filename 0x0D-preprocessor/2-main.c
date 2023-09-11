80c4bcc9a1b2951d987c11102f6df5c167746eb3
gamrani younes <gamrani.younes@gmail.com>
2023-09-11 14:46:23 -0700

    pre processor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,13 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
 
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("%d", file);
+	printf("%s\n", __FILE__);
 	return (0);
 }
 
